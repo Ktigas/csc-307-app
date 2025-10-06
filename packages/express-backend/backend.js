d9d371b6f3ed2e85a5e85e8799047eccb49c5df8
Kyrene Angelica Tigas <angeltigas@Kyrenes-MacBook-Pro.local>
2025-10-06 12:16:41 -0700

    Add hard delete and get users by name and job

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -4,96 +4,97 @@ const app = express();
 const port = 8000;
 
 const users = {
-    users_list: [
-      {
-        id: "xyz789",
-        name: "Charlie",
-        job: "Janitor"
-      },
-      {
-        id: "abc123",
-        name: "Mac",
-        job: "Bouncer"
-      },
-      {
-        id: "ppp222",
-        name: "Mac",
-        job: "Professor"
-      },
-      {
-        id: "yat999",
-        name: "Dee",
-        job: "Aspring actress"
-      },
-      {
-        id: "zap555",
-        name: "Dennis",
-        job: "Bartender"
-      },
-      {
-        "id": "qwe123",
-        "job": "Zookeeper",
-        "name": "Cindy"
-      }
-    ]
-  };
-
-const findUserByName = (name) => {
-return users["users_list"].filter(
-    (user) => user["name"] === name
-);
+  users_list: [
+    { id: "xyz789", name: "Charlie", job: "Janitor" },
+    { id: "abc123", name: "Mac", job: "Bouncer" },
+    { id: "ppp222", name: "Mac", job: "Professor" },
+    { id: "yat999", name: "Dee", job: "Aspring actress" },
+    { id: "zap555", name: "Dennis", job: "Bartender" },
+    { id: "qwe123", name: "Cindy", job: "Zookeeper" }
+  ]
 };
 
-const findUserById = (id) =>
-    users["users_list"].find((user) => user["id"] === id);
 
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
+const findUsersByNameAndJob = (name, job) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name && user["job"] === job
+  );
+};
+
+const findUserById = (id) => {
+  return users["users_list"].find((user) => user["id"] === id);
+};
 
 const addUser = (user) => {
-    users["users_list"].push(user);
-    return user;
-  };
+  users["users_list"].push(user);
+  return user;
+};
 
+const deleteUserById = (id) => {
+  const index = users["users_list"].findIndex((user) => user["id"] === id);
+  if (index !== -1) {
+    users["users_list"].splice(index, 1);
+    return true;
+  }
+  return false;
+};
 
-app.get("/users", (req, res) => {
-const name = req.query.name;
-if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
-} else {
-    res.send(users);
-}
-});
-
-app.get("/users/:id", (req, res) => {
-    const id = req.params["id"]; //or req.params.id
-    let result = findUserById(id);
-    if (result === undefined) {
-      res.status(404).send("Resource not found.");
-    } else {
-      res.send(result);
-    }
-  });
 
 app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.send("Hello World! Have a good day!");
 });
 
 app.get("/users", (req, res) => {
-    res.send(users);
-  });
+  const { name, job } = req.query;
 
+  let result;
+  if (name && job) {
+    result = findUsersByNameAndJob(name, job);
+  } else if (name) {
+    result = findUserByName(name);
+  } else {
+    result = users["users_list"];
+  }
 
-app.post("/users", (req, res) => {
-    const userToAdd = req.body;
-    addUser(userToAdd);
-    res.send();
+  res.send({ users_list: result });
 });
 
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  const result = findUserById(id);
+
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.status(201).send();
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  const success = deleteUserById(id);
+
+  if (success) {
+    res.status(204).send(); // No Content
+  } else {
+    res.status(404).send("User not found.");
+  }
+});
+
+
 app.listen(port, () => {
-  console.log(
-    `Example app listening at http://localhost:${port}`
-  );
+  console.log(`Example app listening at http://localhost:${port}`);
 });
